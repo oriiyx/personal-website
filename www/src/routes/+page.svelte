170bda0eb515b6f359b0ebad2b55888b3599551b
oriiyx <paravinja92@gmail.com>
2025-04-03 17:28:05 +0200

    added terminal functionality

--- www/src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-   import {greet} from '../../../pkg/personal_website';
-
-</script>
-
-<h2>{greet("The Friendly TL")}</h2>
--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,13 +1,11 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
 	plugins: [
-		tailwindcss(),
-		sveltekit(),
+		svelte(),
 		wasm(),
 		topLevelAwait()
 	]
