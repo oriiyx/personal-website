432356cc9c5a3c8537dce79c246e7753c51292b2
oriiyx <paravinja92@gmail.com>
2025-04-03 22:14:45 +0200

    change deploy strat - if it works :thinking:

--- tests/web.rs
+++ tests/web.rs
@@ -7,7 +7,8 @@ use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
-#[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
-}
+// todo - add tests to see how it feels to develop via tests
+// #[wasm_bindgen_test]
+// fn pass() {
+//     assert_eq!(1 + 1, 2);
+// }
